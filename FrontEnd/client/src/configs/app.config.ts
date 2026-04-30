e4721935e38c934453205c6ac49727348de2e91c
NhongPlus <lop10czodoi@gmail.com>
2026-04-30 21:34:44 +0700

    changelink localhost to api.nhongplus

--- FrontEnd/client/src/configs/app.config.ts
+++ FrontEnd/client/src/configs/app.config.ts
@@ -9,10 +9,13 @@ export type AppConfig = {
   locale: string;
   layoutType: LayoutTypes;
 };
-// import.meta.env.VITE_API_URL ||
 const appConfig: AppConfig = {
   layoutType: LayoutTypes.SimpleSideBar,
-  apiURL:  'http://localhost:5000',
+  apiURL: import.meta.env.VITE_API_URL || (
+    window.location.hostname === 'localhost'
+      ? 'http://localhost:5000'
+      : 'https://api.nhongplus.id.vn'
+  ),
   apiPrefix: '/v1',
   authenticatedEntryPath: '/main',
   unAuthenticatedEntryPath: '/login',
